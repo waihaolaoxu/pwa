cb75f8887b172645dc17f3f152f58018bdbc2b17
xushilong <xushilong@dyrs.com.cn>
2020-06-01 16:53:06 +0800

    update

---
+++ dist/precache-manifest.02abc4e78a60c84c019d25f411ec03b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a949328857805f76b616",
+    "url": "/css/app.d5662a18.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "aad24319995b4f1ffb8e269e729984ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c411332f86513af01f72",
+    "url": "/js/about.79ac0b07.js"
+  },
+  {
+    "revision": "a949328857805f76b616",
+    "url": "/js/app.9d4d5d93.js"
+  },
+  {
+    "revision": "35fc30c118265c969b43",
+    "url": "/js/chunk-vendors.fd37fb4f.js"
+  },
+  {
+    "revision": "b0d6ded906db037aaed0ff85b3eba813",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
